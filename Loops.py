94fb69b26357fa3e2d1e714a19d18dc4a0df5b3b
Pranali Valve <96332685+pranalivalve1108@users.noreply.github.com>
2025-11-30 16:49:55 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,9 @@
+#For loop
+print("**For loop**")
+for i in range(5): print(i)
+count = 1
+ # While
+print("**While loop**")
+while count <= 5:
+    print(count)
+    count += 1
---
+++ Variables.py
@@ -0,0 +1,4 @@
+name= " Python Programming"
+age = 20
+price = 200.30
+is_stduent = True
