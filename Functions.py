927d4cffdeb55204aaf5106406c7650af754534c
Pranali Valve <96332685+pranalivalve1108@users.noreply.github.com>
2025-12-01 20:14:35 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,18 @@
+#Create Function
+def app():
+    print("Welcome to the Python Learning Journey...!!!")
+#Calling the Function
+app()
+
+
+#Function with Parameters
+def add(a, b):
+    return(a + b)
+print(add(5, 5))
+
+
+#Function with default values
+def display_results(name="Guest"):
+        print("Welcome, ", name)
+display_results("Pranali")
+
