a4f6b9be632ccc73b96633c5210af7b2461a1b86
Pranali Valve <96332685+pranalivalve1108@users.noreply.github.com>
2025-12-10 12:06:52 +0530

    Sets in Python

---
+++ Sets.py
@@ -0,0 +1,13 @@
+#Creating a Set
+nums = {1, 2, 3, 3, 4}
+print(nums)
+
+#Empty set
+empty_set = set()
+print(empty_set)
+
+#Adding and Removing elements
+print(nums.add(5))
+print(nums.remove(2))
+print(nums.discard(10))
+print(nums.pop())
